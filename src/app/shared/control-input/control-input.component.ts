cbc40d980019fa0bc96e5a16912e6417cad511af
Ewerthon Santana <ewerthon32santana@gmail.com>
2022-04-24 19:43:21 -0300

    disgraça do krl

--- src/app/shared/control-input/control-input.component.ts
+++ src/app/shared/control-input/control-input.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, ContentChild, Input, OnInit } from '@angular/core';
+import { FormControlName } from '@angular/forms';
 
 @Component({
   selector: 'app-control-input',
@@ -9,11 +10,25 @@ export class ControlInputComponent implements OnInit {
 
   @Input() label: string;
   @Input() errorMessage: string;
+  input: any;
 
+  @ContentChild(FormControlName) control: FormControlName;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  ngAfterContentInit() {
+    this.input = this.control
+  }
+
+  hasSuccess(): boolean {
+    return this.input.valid && this.input.touched;
+  }
+
+  hasError(): boolean {
+    return this.input.invalid && this.input.touched;
   }
 
 }
